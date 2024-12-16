8b0d55d6fa63984d0c33105eb42fbcf686e09816
dfgh2727 <dfgh2727@naver.com>
2024-12-16 12:57:45 +0900

    이수환: 내용 이동중

---
+++ Downwell_DX/EngineBase/PreCompile.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// os
+#include <Windows.h>
+
+// std
+#include <list>
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+#include <functional>
+
+// 쉐어드 Ptr을 사용하기 위한 헤더
+#include <memory>
+
+// 수학
+#include "EngineMath.h"
+#include "EngineDirectory.h"
+#include "EngineFile.h"
+#include "EngineSerializer.h"
+#include "EnginePath.h"
+#include "EngineDebug.h"
+#include "EngineDefine.h"
