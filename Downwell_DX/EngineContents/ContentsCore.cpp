d15838d663e755df418155a459615a80f500ce34
dfgh2727 <dfgh2727@naver.com>
2024-12-17 17:34:55 +0900

    이수환: 엔진 업데이트 (241217_1734)

--- Downwell_DX/EngineContents/ContentsCore.cpp
+++ Downwell_DX/EngineContents/ContentsCore.cpp
@@ -14,20 +14,20 @@ UContentsCore::~UContentsCore()
 {
 }
 
+
 void UContentsCore::EngineStart(UEngineInitData& _Data)
 {
 	// mainwindow는 아무나 건들면 안된다.
 	// 넌 컨텐츠잖아 엔진이 관리하는 윈도우라는게 존재하는지도 몰라야한다.
 
 	_Data.WindowPos = { 100, 100 };
-	_Data.WindowSize = { 300, 300 };
+	_Data.WindowSize = { 1280, 720 };
 
-	// static으로 
-	// 이 된다.
+	// 리로스 로드 할수 없다.
+
+	// UEngineCore::CreateLevel<ATitleGameMode, APawn>("Titlelevel");
 	UEngineCore::CreateLevel<ATitleGameMode, APawn>("Titlelevel");
 	UEngineCore::OpenLevel("Titlelevel");
-
-	// 윈도우 크기 지정
 	
 }
 
