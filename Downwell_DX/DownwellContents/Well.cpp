d2ea54d45277d3e1eeb3fa3de4348f5008ec4829
dfgh2727 <dfgh2727@naver.com>
2025-02-04 10:12:41 +0900

    이수환: Title Collision Box 위치 조정

--- Downwell_DX/DownwellContents/Well.cpp
+++ Downwell_DX/DownwellContents/Well.cpp
@@ -18,25 +18,25 @@ Well::Well()
 	CollisionBox1->SetupAttachment(RootComponent);
 	CollisionBox1->SetCollisionProfileName("Block");
 	CollisionBox1->SetScale3D({ 96.0f, 160.0f });
-	CollisionBox1->SetRelativeLocation({ -118.0f, 0.0f, 0.0f });
+	CollisionBox1->SetRelativeLocation({ -118.0f, 8.0f, 0.0f });
 
 	CollisionBox2 = CreateDefaultSubObject<UCollision>();
 	CollisionBox2->SetupAttachment(RootComponent);
 	CollisionBox2->SetCollisionProfileName("Block");
 	CollisionBox2->SetScale3D({ 96.0f, 160.0f });
-	CollisionBox2->SetRelativeLocation({ 118.0f, 0.0f, 0.0f });
+	CollisionBox2->SetRelativeLocation({ 118.0f, 8.0f, 0.0f });
 
 	CollisionBox3 = CreateDefaultSubObject<UCollision>();
 	CollisionBox3->SetupAttachment(RootComponent);
 	CollisionBox3->SetCollisionProfileName("Block");
 	CollisionBox3->SetScale3D({ 54.0f, 48.0f });
-	CollisionBox3->SetRelativeLocation({ -100.0f, 96.0f, 0.0f });
+	CollisionBox3->SetRelativeLocation({ -97.0f, 100.0f, 0.0f });
 
 	CollisionBox4 = CreateDefaultSubObject<UCollision>();
 	CollisionBox4->SetupAttachment(RootComponent);
 	CollisionBox4->SetCollisionProfileName("Block");
 	CollisionBox4->SetScale3D({ 54.0f, 48.0f });
-	CollisionBox4->SetRelativeLocation({ 100.0f, 96.0f, 0.0f });
+	CollisionBox4->SetRelativeLocation({ 97.0f, 100.0f, 0.0f });
 }
 
 Well::~Well()
