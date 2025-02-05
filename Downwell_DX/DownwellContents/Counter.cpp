7c4261104f8d4e99a3e519f90dda40b02858e98f
dfgh2727 <dfgh2727@naver.com>
2025-02-05 18:05:30 +0900

    이수환: Counter

---
+++ Downwell_DX/DownwellContents/Counter.cpp
@@ -0,0 +1,93 @@
+#include "PreCompile.h"
+#include "Counter.h"
+#include <EngineCore/DefaultSceneComponent.h>
+
+
+Counter::Counter()
+{
+	std::shared_ptr<UDefaultSceneComponent> Default = CreateDefaultSubObject<UDefaultSceneComponent>();
+	RootComponent = Default;
+
+	MinusRenderer = CreateDefaultSubObject<USpriteRenderer>();
+	MinusRenderer->SetupAttachment(RootComponent);
+	MinusRenderer->SetSprite("CMnumMinus.png");
+
+	for (size_t i = 0; i < 12; i++)
+	{
+		// 카메라가 움직여도 이녀석은 움직이지 않는다.
+		USpriteRenderer* Sprite = CreateDefaultSubObject<USpriteRenderer>();
+		Sprite->SetCameraEffect(false);
+		NumRenderer.push_back(Sprite);
+	}
+}
+
+Counter::~Counter()
+{
+}
+
+void Counter::BeginPlay()
+{
+}
+
+void Counter::Tick(float _DeltaTime)
+{
+
+}
+
+void Counter::SetTextSpriteName(const std::string _Text)
+{
+	TextSpriteName = _Text;
+
+	for (size_t i = 0; i < NumRenderer.size(); i++)
+	{
+		NumRenderer[i]->SetSprite(TextSpriteName);
+	}
+}
+
+void Counter::SetOrder(int _Order)
+{
+	for (size_t i = 0; i < NumRenderer.size(); i++)
+	{
+		NumRenderer[i]->SetOrder(_Order);
+	}
+
+}
+
+void Counter::SetValue(int _Score)
+{
+	std::string Number = std::to_string(_Score);
+
+	if (NumRenderer.size() <= Number.size())
+	{
+		MSGASSERT("자리수를 넘겼습니다.");
+		return;
+	}
+
+
+	FVector Pos = FVector::ZERO;
+
+	for (size_t i = 0; i < Number.size(); i++)
+	{
+		char Value = Number[i] - '0';
+		NumRenderer[i]->SetSprite(TextSpriteName, Value);
+		NumRenderer[i]->SetComponentScale(TextScale);
+		NumRenderer[i]->SetComponentLocation(Pos);
+		Pos.X += TextScale.X;
+		NumRenderer[i]->SetActive(true);
+	}
+
+	MinusRenderer->SetComponentLocation({ -TextScale.X, 0.0f});
+
+	for (size_t i = Number.size(); i < NumRenderer.size(); i++)
+	{
+		NumRenderer[i]->SetActive(false);
+	}
+
+}
+
+void Counter::ShowMinus()
+{
+	MinusRenderer->SetOrder(ERenderOrder::UI);
+	MinusRenderer->SetComponentScale({ 25, 50 });
+}
+
---
+++ Downwell_DX/DownwellContents/Counter.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <vector>
+#include <EngineCore/SpriteRenderer.h>
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class Counter : public AActor
+{
+public:
+	// constrcuter destructer
+	Counter();
+	~Counter();
+
+	// delete Function
+	Counter(const Counter& _Other) = delete;
+	Counter(Counter&& _Other) noexcept = delete;
+	Counter& operator=(const Counter& _Other) = delete;
+	Counter& operator=(Counter&& _Other) noexcept = delete;
+
+	void SetTextSpriteName(const std::string _Text);
+
+	void SetTextScale(FVector _TextScale)
+	{
+		TextScale = _TextScale;
+	}
+
+	template<typename EnumType>
+	void SetOrder(EnumType _Order)
+	{
+		SetOrder(static_cast<int>(_Order));
+	}
+
+	void SetOrder(int _Order);
+
+	void SetValue(int _Score);
+
+	void ShowMinus();
+
+
+protected:
+
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+	std::string TextSpriteName;
+	FVector TextScale;
+	std::shared_ptr<class USpriteRenderer> MinusRenderer;
+	std::vector<class USpriteRenderer*> NumRenderer;
+
+};
+
